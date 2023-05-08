fc28c9a321c265ce96f6bc632d0ec31b973393c6
daviinnchi <ivanzaiko99@gmail.com>
2023-05-08 11:58:53 +0200

    CalculateWinner now also returns a line of a winner

--- src/helpers/calculateWinner.js
+++ src/helpers/calculateWinner.js
@@ -13,10 +13,7 @@ export default function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      for (let ch of lines[i]) {
-        document.getElementById(ch).style.backgroundColor = "green";
-      }
-      return squares[a];
+      return squares[a], a, b, c;
     }
   }
   return null;
