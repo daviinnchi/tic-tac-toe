3d22c041337429f98fda20a6d2a6bfeee1859d75
daviinnchi <ivanzaiko99@gmail.com>
2023-05-08 11:20:59 +0200

    for statement changed to forEach()

--- src/helpers/getAllIndexes.js
+++ src/helpers/getAllIndexes.js
@@ -1,6 +1,7 @@
 export default function getAllIndexes(arr, val) {
     var indexes = [],
       i;
-    for (i = 0; i < arr.length; i++) if (arr[i] === val) indexes.push(i);
+    // for (i = 0; i < arr.length; i++) if (arr[i] === val) indexes.push(i);
+    indexes.forEach(() => indexes.push(val));
     return indexes;
   }
