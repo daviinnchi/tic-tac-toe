4c221e22573635b44b80abe21eb8b1d280b8399e
daviinnchi <ivanzaiko99@gmail.com>
2023-05-16 08:17:16 +0200

    Deleted unused code from all files

--- src/helpers/calculateWinner.js
+++ src/helpers/calculateWinner.js
@@ -1,7 +1,6 @@
 import { lines } from "./lines";
 
 export default function calculateWinner(squares) {
-
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
