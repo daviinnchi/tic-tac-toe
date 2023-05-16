dcd06b9d1725c49e9d892ad98ada0440e5edf5f4
daviinnchi <ivanzaiko99@gmail.com>
2023-05-16 10:20:35 +0200

    Added GameInfo.jsx component, which displays info about the game

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { locations } from "./helpers/locations";
 import Board from "./components/Board/Board";
 import getAllIndexes from "./helpers/getAllIndexes";
 import { reducer } from "./components/reducer";
+import { GameInfo } from "./components/GameInfo";
 
 export default function Game() {
   const [state, dispatch] = useReducer(reducer, {
@@ -16,31 +17,6 @@ export default function Game() {
   function handlePlay(nextSquares) {
     dispatch({ type: "handlePlay", payload: nextSquares });
   }
-  function jumpTo(nextMove) {
-    dispatch({ type: "jump_to_move", payload: nextMove });
-  }
-  const moves = state.history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      move === state.currentMove
-        ? (description = "You are at move #" + move)
-        : (description = "Go to move #" + move);
-    } else {
-      description = "Go to game start";
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
-  useEffect(() => {
-    dispatch({ type: "new_X_move", payload: currentSquares });
-    dispatch({ type: "new_O_move", payload: currentSquares });
-  }, [currentSquares]);
-  if (!state.sortAsc) {
-    moves.reverse();
-  }
   return (
     <div className="game">
       <div className="game-board">
@@ -50,27 +26,15 @@ export default function Game() {
           currentMove={state.currentMove}
         />
       </div>
-      <div className="game-info">
-        <button
-          className="sort-btn"
-          onClick={() => dispatch({ type: "sort_history", payload: true })}
-        >
-          Sort by ascending order
-        </button>
-        <button
-          className="sort-btn"
-          onClick={() => dispatch({ type: "sort_history", payload: false })}
-        >
-          Sort by descending order
-        </button>
-        <ol>{moves}</ol>
-        <div className="moves-info">
-          'X' player moves <br /> {state.xMoves}
-        </div>
-        <div className="moves-info">
-          'O' player moves <br /> {state.oMoves}
-        </div>
-      </div>
+      <GameInfo 
+        history={state.history}
+        currentMove={state.currentMove}
+        currentSquares={currentSquares}
+        sortAsc={state.sortAsc}
+        xMoves={state.xMoves}
+        oMoves={state.oMoves}
+        dispatch={dispatch}
+      />
     </div>
   );
 }
