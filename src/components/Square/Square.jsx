3cf6bcc64a1bee0ca3facab744a40a9ea91faf15
daviinnchi <ivanzaiko99@gmail.com>
2023-05-16 10:36:16 +0200

    Changed class names in Square.jsx

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -3,7 +3,7 @@ import "./Square.css";
 export const Square = ({ value, handleClick, isWinner }) => {
   return (
     <button
-      className={isWinner ? "square-winner" : "square"}
+      className={isWinner ? "board_row board_row__square square_type_winner" : "board_row__square"}
       onClick={handleClick}
     >
       {value}
